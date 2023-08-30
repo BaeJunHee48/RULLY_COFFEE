6ea5743c9f7d166dc87026aefa691c8ea736edbf
owlwit <selfcontrol1356@gmail.com>
2023-08-30 18:03:18 +0900

    feat : 서브페이지 1  관련 html, css, js 입니다.

--- js/sub_page1.js
+++ js/sub_page1.js
@@ -55,3 +55,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// 페이지 로딩시 메인사진 글자에 밑줄 효과
+document.addEventListener("DOMContentLoaded", function () {
+  const imageOverlay = document.querySelector(".image-overlay");
+  // 원하는 시간(밀리초) 만큼 지연
+  // setTimeout(function() {
+  //   imageOverlay.classList.add("loaded");
+  // }, 1000); // 1000밀리초 = 1초
+  imageOverlay.classList.add("loaded");
+});
